7f010b9084874a2df41be45f5b44f596e5b380a4
Touracht <tebogomataba6810@gmail.com>
2024-08-17 01:01:59 +0200

    Add new folder django-models with a djangoapp

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,5 +1,3 @@
-from .models import Author, Book, Library, Librarian
-
 books = "Author.objects.get(name=author_name)", "objects.filter(author=author)"
 
 books = "Library.objects.get(name=library_name), books.all()"
