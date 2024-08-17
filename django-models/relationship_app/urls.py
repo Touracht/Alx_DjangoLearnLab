e326255bbcaa0f252eeb65123cde2b954d95ef6a
Touracht <tebogomataba6810@gmail.com>
2024-08-17 20:57:36 +0200

    Add to relationship_app in django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
-from .views import register
+from . import views
 
 urlpatterns = [
     path('listbooks/', list_books, name = 'list_books'),
     path('librarydetail/', LibraryDetailView.as_view(), name = 'library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
-    path('register/', register.as_view(), name = "register")
+    path('logout/', LogoutView.as_view(template_name = ""), name='logout'),
+    path('register/', views.register.as_view(), name = "register")
 
 ]
