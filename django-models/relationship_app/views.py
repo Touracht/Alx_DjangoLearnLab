15d4b0d435d788132695e4a2b34c6ed23cea5009
Touracht <tebogomataba6810@gmail.com>
2024-08-17 21:02:52 +0200

    Add to relationship_app in django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -17,9 +17,9 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
 
 class register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
-    template_name = 'relationship_app/signup.html'
+    template_name = 'relationship_app/register.html'
 
 
 
