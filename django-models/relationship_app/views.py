adc62d0a4ec3c9d5409b6c0fb12e28e69cf8a05c
Touracht <tebogomataba6810@gmail.com>
2024-08-17 19:45:26 +0200

    Add to relationship_app in django-models

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.views.generic import DetailView
 def list_books(request):
     books = models.Book.objects.all()
     context = {'books': books}
-    return render(request,'list_books.html', context)
+    return render(request,'relationship_app/list_books.html', context)
 
 class library_detail(DetailView):
     model = models.Library
