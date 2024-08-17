e570bec577cdc61b5c8248bd0cff25a30e8f8d40
Touracht <tebogomataba6810@gmail.com>
2024-08-17 21:00:04 +0200

    Add to relationship_app in django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('listbooks/', list_books, name = 'list_books'),
     path('librarydetail/', LibraryDetailView.as_view(), name = 'library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = ""), name='logout'),
+    path('logout/', LogoutView.as_view(template_name= "relationship_app/logout.html"), name='logout'),
     path('register/', views.register.as_view(), name = "register")
 
 ]
