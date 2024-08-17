3ecc2122e75165da1a9425b7bc18b673ae0738b0
Touracht <tebogomataba6810@gmail.com>
2024-08-17 19:54:20 +0200

    Add to relationship_app in django-models

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('listbooks/', list_books, name = 'list_books'),
