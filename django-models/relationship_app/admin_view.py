8880ee644d720e6fd8bf244a5435bdd903db2074
Touracht <tebogomataba6810@gmail.com>
2024-08-18 18:05:25 +0200

    Add admin_view.py, librarian_view.py and member_view.py to relationship_app

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from django.http import HttpResponseForbidden
 
-# Helper function to check if the user is an Admin
 def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
 
