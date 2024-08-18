3bb3dfaec0e9dbba5327596663e2fa208e792975
Touracht <tebogomataba6810@gmail.com>
2024-08-18 20:06:25 +0200

    Add admin_view.py, librarian_view.py and member_view.py to relationship_app

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import user_passes_test
 from .role_checks import is_admin  # assuming role_checks.py contains your role checking functions
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     # Your logic for admin view
     return render(request, 'admin_template.html')  # replace with your template
