ab90dba122925a3946fea000ff1ee1589ed94901
Touracht <tebogomataba6810@gmail.com>
2024-08-18 22:46:02 +0200

    Add admin_view.py, librarian_view.py and member_view.py to relationship_app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -24,7 +24,7 @@ class register(CreateView):
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
-def admin_view(user):
+def is_admin(user):
     return user.userprofile.role == 'Admin'
 
 def is_librarian(user):
@@ -32,7 +32,7 @@ def is_librarian(user):
 
 def is_member(user):
     return user.userprofile.role == 'Member'
-@user_passes_test(admin_view)
+@user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
