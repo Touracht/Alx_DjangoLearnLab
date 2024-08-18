b65960198633873e32112560524e0530cfb703b6
Touracht <tebogomataba6810@gmail.com>
2024-08-18 23:32:51 +0200

    Add admin_view.py, librarian_view.py and member_view.py to relationship_app

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -10,6 +10,8 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    class Meta: 
+        permissions = [('can_add_book, Can add Book'), ('can_change_book, Can change book'), ('can_delete_book, Can delete book')]
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -32,6 +32,7 @@ def is_librarian(user):
 
 def is_member(user):
     return user.userprofile.role == 'Member'
+
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
