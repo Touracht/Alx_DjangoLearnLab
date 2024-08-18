8e6973107d6d8fd5675f8ee88e37d64bfa59f30f
Touracht <tebogomataba6810@gmail.com>
2024-08-18 19:30:42 +0200

    Add admin_view.py, librarian_view.py and member_view.py to relationship_app

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,13 +1,10 @@
+# admin_view.py
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
-from django.http import HttpResponseForbidden
+from django.http import HttpResponse
 
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
+    return user.groups.filter(name='Admin').exists()
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_page.html')
-
-# def handle_forbidden(request):
-#     return HttpResponseForbidden("You are not allowed to access this page.")
+    return HttpResponse("Welcome to the Admin View.")
