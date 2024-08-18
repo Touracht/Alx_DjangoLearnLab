1f3980e59e55dbad18f58329d7d93a886cc1aad0
Touracht <tebogomataba6810@gmail.com>
2024-08-18 21:00:10 +0200

    Add admin_view.py, librarian_view.py and member_view.py to relationship_app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('librarydetail/', LibraryDetailView.as_view(), name = 'library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name= "relationship_app/logout.html"), name='logout'),
-    path('register/', views.register.as_view(), name = "register")
+    path('register/', views.register.as_view(), name = "register"),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
