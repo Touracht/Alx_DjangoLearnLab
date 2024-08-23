5ef319845b2604546586d35b2407066e2fec7fb5
Touracht <tebogomataba6810@gmail.com>
2024-08-24 01:35:20 +0200

    Add a new project 'advanced_features_and_security'

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i^noki=j%x@8=xh%6lbncjuag@=#h_ad+(=es2hr8ekpa(pjwy'
+SECRET_KEY = 'django-insecure-ng7yxx(iyd&f%o749^i1l@dho3=)hc@!+4151m2+$bsw4a8h#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf',
-    'relationship_app'
+    'bookshelf'
 ]
 
 MIDDLEWARE = [
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -30,6 +30,6 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=False)
     profile_photo = models.ImageField()
 
-class Permissions(models.model):
+class Permissions(models.Model):
     premissions = [('can_view', 'Can view'), ('can_create', 'Can view'),
                    ('can _edit', 'Can edit'), ('can_delete', 'Can delete')]
