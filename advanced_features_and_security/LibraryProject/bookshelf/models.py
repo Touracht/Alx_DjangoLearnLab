6850bdda9b93e3eda877ce7061d0ab29aebb538f
Touracht <tebogomataba6810@gmail.com>
2024-08-24 00:23:18 +0200

    Add a new project 'advanced_features_and_security'

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -4,3 +4,28 @@ class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 200)
     publication_year = models.IntegerField()
+
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('The Email field must be set')
+        user = self.model(email = self.normalize_email(email))
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using = self._db)
+        return user
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=False)
+    profile_photo = models.ImageField()
