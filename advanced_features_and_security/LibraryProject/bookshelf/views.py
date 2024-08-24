75d13527f8e04af01d341eb8f9f21ff2b7832bae
Touracht <tebogomataba6810@gmail.com>
2024-08-24 20:51:33 +0200

    Add a new project 'advanced_features_and_security'

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
+from django import forms
+from .models import Book
 
-# @permission_required('bookshelf.can_view')
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+@permission_required('bookshelf.can_view', raise_exception = True)
+def create_book(request):
+    if request == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BookForm
+
+@permission_required('bookshelf.can_edit', raise_exception = True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk = pk)
+    if request == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid:
+            form.save()
+
+    else:
+        form = BookForm(instance=book)
+
+@permission_required('bookshelf.can_delete', raise_exception = True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk = pk)
+    if request == 'POST':
+        book.delete()
+
+
+
+
+    
