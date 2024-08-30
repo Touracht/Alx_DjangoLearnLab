8c2f3a0d08c6317901782c49dfbd45bf14738380
Touracht <tebogomataba6810@gmail.com>
2024-08-30 04:02:26 +0200

    Create a new django project named api_project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.CreateAPIView):
     queryset = Book.objects.all()
     seraizer_class =BookSerializer
 
