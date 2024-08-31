545c222bfb47b005e2f9f1a9688695c398eb9aa5
Touracht <tebogomataba6810@gmail.com>
2024-08-31 18:08:11 +0200

    Create a new django project named api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .models import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'my-models', BookViewSet)
 
 urlpatterns = [
-    path("booklist/", BookList.as_view(),name = 'book_list')
+    # path("booklist/", BookList.as_view(),name = 'book_list')
+    path('api/', include(router.urls))
+
+
 ]
