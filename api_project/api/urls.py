ef31b623bc29000d068ea0a00ddd2e42a82d98c6
Touracht <tebogomataba6810@gmail.com>
2024-09-01 20:36:50 +0200

    Create a new django project named api_project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'my-models', BookViewSet)
 
 urlpatterns = [
-    # path("booklist/", BookList.as_view(),name = 'book_list')
-    path('api/', include(router.urls))
-
-
+    path('api/', include(router.urls)),
+    path('api/token/', obtain_auth_token, name = 'api_token_auth')
 ]
