27cbf4e2a4ca26e983a58ab4e222ae18dfb21f30
Touracht <tebogomataba6810@gmail.com>
2024-09-06 01:30:13 +0200

    Add a new django project named advanced-api-project

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 class Author(models.Model):
+    """This model is for creating authors who are are able to create books using the book model below.
+    Each author will be identified by their unique key together with their name."""
     name = models.CharField(max_length=50)
 
 class Book(models.Model):
+    """This model is for creating book objects with auto_generated keys, title, publication_year and lastly the author
+    field which references to the Author model as a foreign key. This will ensure that each author can create
+    multiple books."""
     title = models.CharField(max_length=100)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -3,6 +3,10 @@ from .models import Author, Book
 from datetime import datetime
 
 class BookSerializer(serializers.ModelSerializer):
+    """This serializer is for converting the Book model instances into python code that will 
+    easly be converted into more human readable formats like JSON and XML. It serializes
+    all the fields from the Book model as well and adds a validation method to handle the year
+    field so it is not in future."""
 
     class Meta:
         model = Book
@@ -16,11 +20,17 @@ class BookSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Publication year must be current.')
 
 class AuthorSerializer(serializers.ModelSerializer):
+    """This serializer is for converting Book model instances into python code that will 
+    easly be converted into more human readable formats like JSON and XML. It includes an
+    additional 'books' field which is responsible for showing all the books that belong to the paricular author"""
     books = BookSerializer(many=True, read_only=True)
     
     class Meta:
         model = Author
         fields = ['name', 'books']
+
+"""The relationship between the Author and the Book models in my serializers is handled by an additional field
+'books' which is able to show books that have been created by a particular author"""
 
 
         
