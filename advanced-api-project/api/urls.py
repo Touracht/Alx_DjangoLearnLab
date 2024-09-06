c64a26edc5bcbcfd144745caf85a15a1fabf68bb
Touracht <tebogomataba6810@gmail.com>
2024-09-06 02:36:08 +0200

    Add a new django project named advanced-api-project

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 urlpatterns = [
-    path('listview/', ListView.as_view(), name = 'list_view'),
-    path('detailview/', DetailView.as_view(), name = 'detail_view'),
-    path('createview/', CreateView.as_view(), name = 'detail_view'),
-    path('updateview/', UpdateView.as_view(), name = 'detailview'),
-    path('deleteview/', DeleteView.as_view(), name = 'delete_view'),
+    path('books', ListView.as_view(), name = 'list_view'),
+    path('books/detail', DetailView.as_view(), name = 'detail_view'),
+    path('books/create', CreateView.as_view(), name = 'detail_view'),
+    path('books/update', UpdateView.as_view(), name = 'detailview'),
+    path('books/delete', DeleteView.as_view(), name = 'delete_view'),
 ]
