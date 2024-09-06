2cc87fa5d72db5fe2e56734ac86034b6b9a41fa5
Touracht <tebogomataba6810@gmail.com>
2024-09-06 02:23:02 +0200

    Add a new django project named advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DetailView(generics.GenericAPIView):
+    def get_book(self, response, pk):
+        try:
+            book = Book.objects.all(pk = pk)
+            serializer = BookSerializer(book)
+            return Response(serializer.data)
+        
+        except Book.DoesNotExist:
+            return Response('Book not found.')
+
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+
+
