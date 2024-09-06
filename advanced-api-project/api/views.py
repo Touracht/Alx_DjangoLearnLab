4b57e1a6481b8532df70f29e43f255e624bcee9b
Touracht <tebogomataba6810@gmail.com>
2024-09-06 21:22:12 +0200

    Add a new django project named advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Book, Author
-from .serializers import BookSerializer, AuthorSerializer
+from .serializers import BookSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DetailView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_book(self, response, pk):
         try:
             book = Book.objects.all(pk = pk)
@@ -21,14 +25,17 @@ class DetailView(generics.GenericAPIView):
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
 
