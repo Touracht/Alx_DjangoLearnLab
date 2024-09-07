63aed32fa685d7c336b82d96a89cb08685bc26bf
Touracht <tebogomataba6810@gmail.com>
2024-09-07 21:18:31 +0200

    Add a new django project named advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,6 +8,7 @@ from rest_framework.exceptions import NotFound
 from .filters import BookFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 
 class ListView(generics.ListAPIView):
 
