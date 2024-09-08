d8dbdc3ffeb5f1fff3062c0d638c82bc3015195c
Touracht <tebogomataba6810@gmail.com>
2024-09-08 17:06:47 +0200

    Add a new django project named advanced-api-project

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from .models import Book
 
-class BookModelTestCase(TestCase):
+class APITestCase(TestCase):
     def setUp(self):
         # Initialize the API client
         self.client = APIClient()
