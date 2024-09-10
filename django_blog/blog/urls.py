2c88831a27d30c0f79f6c1084a569bb024563ddb
Touracht <tebogomataba6810@gmail.com>
2024-09-10 22:44:33 +0200

    Add a new Django project named django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import register, login, LogOut, HomePageView, profile_management
+from .views import register, login_view, LogOut, HomePageView, profile_management
 
 urlpatterns = [
     path('home', HomePageView.as_view(), name = 'home'),
     path('register/', register, name = 'register'),
-    path('login/', login, name = 'login'),
+    path('login/', login_view, name = 'login'),
     path('logout/', LogOut.as_view(), name = 'logout'),
     path('profile/', profile_management, name = 'profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, UserProfileForm
@@ -10,7 +10,7 @@ from django.urls import reverse_lazy
 
 def register(request):
     if request.method == "POST":
-        form = CustomUserCreationForm()
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit = False)
             user.email = form.cleaned_data.get('email')
@@ -19,43 +19,42 @@ def register(request):
     
     else:
         form = CustomUserCreationForm()
-    return render(request, 'blog/register.html', {'from': form})
+    return render(request, 'blog/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
-
+            auth_login(request, user)
             return redirect('home')
     else:
         form = AuthenticationForm()
     return render(request, 'blog/login.html', {'form': form})
 
-@login_required()
+@login_required
 def profile_management(request):
     if request.method == 'POST':
-        user_form = CustomUserCreationForm(request.POST, instance = request.user)
-        profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        user_form = CustomUserCreationForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST)
 
-        if user_form.is_valid() and profile_form.is_valid():user_form.save() 
-        profile_form.save()
-
-        return redirect('home')
-    
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('home')
     else:
-        user_form = CustomUserCreationForm(instance = request.user)
-        profile_form = UserProfileForm(instance=request.user.userprofile)
+        # Initialize the forms with existing data
+        user_form = CustomUserCreationForm(instance=request.user)
+        profile_form = UserProfileForm()
 
-    return render(request, 'blog/profile.html', {'user_form':user_form, 'profile_form': profile_form})
+    return render(request, 'blog/profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
 class LogOut(LogoutView):
     template_name = 'blog/logout.html'
     successful_url = reverse_lazy('home')
 
 class HomePageView(TemplateView):
-    template_name = 'blog/home.html'
+    template_name = 'blog/base.html'
     
 
 
