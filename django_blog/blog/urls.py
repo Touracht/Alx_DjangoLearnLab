f687a9549e432e6dc31c40ea860a63efc45168bc
Touracht <tebogomataba6810@gmail.com>
2024-09-10 03:34:27 +0200

    Add a new Django project named django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,8 @@ from .views import register, login, LogOut, HomePageView, profile_management
 
 urlpatterns = [
     path('home', HomePageView.as_view(), name = 'home'),
-    path('register', register, name = 'register'),
-    path('login', login, name = 'login'),
-    path('logout', LogOut.as_view(), name = 'logout'),
-    path('profile', profile_management, name = 'profile'),
+    path('register/', register, name = 'register'),
+    path('login/', login, name = 'login'),
+    path('logout/', LogOut.as_view(), name = 'logout'),
+    path('profile/', profile_management, name = 'profile'),
 ]
