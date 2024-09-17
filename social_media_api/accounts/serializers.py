3d44cad8077dbc2a573cbf1b1fc5a9ddce6f322f
Touracht <tebogomataba6810@gmail.com>
2024-09-18 01:35:47 +0200

    Add django project named 'social_media_api'

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth import authenticate
 
 class CustomRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -13,4 +14,28 @@ class CustomRegisterSerializer(serializers.ModelSerializer):
         
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = CustomUser(username = validated_data['username'])
+        user = CustomUser(username = validated_data['username'],
+                          bio = validated_data.get('bio', ''),
+                          profile_picture = validated_data.get('profile_picture', None))
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError('Invalid username or password')
+        
+        data['user'] = user
+        return data
+        
