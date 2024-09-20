21fb8aa65c1cb0f02e93a4942c58495531ee7751
Touracht <tebogomataba6810@gmail.com>
2024-09-21 01:09:12 +0200

    Add django project named 'social_media_api'

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -37,7 +37,6 @@ from .models import CustomUser
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-
 class FollowView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
