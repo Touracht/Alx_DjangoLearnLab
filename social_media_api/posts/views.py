a91f577eaf3fac301cc77b96e7cc7bddc5d27d30
Touracht <tebogomataba6810@gmail.com>
2024-09-21 01:00:07 +0200

    Add django project named 'social_media_api'

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import permissions
 from .serializers import PostSerializer, CommentSerializer
 from .models import Post, Comment
 from django.db.models import Q
@@ -11,15 +11,15 @@ from rest_framework import generics
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     pagination_class = [CustomPostPagination]
 
     def get_permissions(self):
         if self.action == 'list':
-            permission_classes = [AllowAny]
+            permission_classes = [permissions.AllowAny]
 
         else:
-            permission_classes = [IsAuthenticated]
+            permission_classes = [permissions.IsAuthenticated]
 
         return [permission() for permission in permission_classes]
     
@@ -36,17 +36,17 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     pagination_class = [CustomCommentPagination]
         
 class FeedView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         user = request.user
 
-        followed_users = user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
