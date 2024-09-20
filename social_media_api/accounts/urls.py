12c59333fe6a99a73324606cf874ee415d6329a5
Touracht <tebogomataba6810@gmail.com>
2024-09-21 01:01:49 +0200

    Add django project named 'social_media_api'

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('register/', register_user, name = 'register'),
     path('login/', login_user,name = 'login'),
     path('profile/', ProfileView.as_view(), name = 'profile'),
-    path('follow/<str:username>/', FollowView.as_view(), name = 'follow_user'),
-    path('unfollow/<str:username>/', UnfollowView.as_view(), name = 'unfollow_user'),
+    path('follow/<int:user_id>/', FollowView.as_view(), name = 'follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowView.as_view(), name = 'unfollow_user'),
 ]
